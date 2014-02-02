0c80367195cc36274f677e588ff4b726b790a846
David Winter <dawi2332@gmail.com>
2014-02-02 15:22:47 +0100

    Added README.md

--- home/.oh-my-zsh/custom/plugins/kbd/kbd.plugin.zsh
+++ home/.oh-my-zsh/custom/plugins/kbd/kbd.plugin.zsh
@@ -1,4 +1,4 @@
-# David Winter <dawi2332@gmail.com>
+# kbd plugin for oh-my-zsh by David Winter <dawi2332@gmail.com>
 
 kbd_zkbd_setup() {
     echo "Failed to read your zkbd key settings from $1."
@@ -12,11 +12,10 @@ kbd_zkbd_setup() {
         echo
         autoload zkbd && zkbd
         export DISPLAY=$old_display
-        unset $old_display
     fi
 }
 
-kbd_zkbd_init() {
+kbd_zkbd() {
     local zkbd_keyfile=${ZDOTDIR}/.zkbd/$TERM-${${DISPLAY#${DISPLAY%:[0-9]*}}:-$VENDOR-$OSTYPE}
     if [[ ! -f $zkbd_keyfile ]]
     then
@@ -25,8 +24,21 @@ kbd_zkbd_init() {
     source $zkbd_keyfile
 }
 
-kbd_terminfo_init() {
+# Setup keys according to terminfo database.
+kbd_terminfo() {
     typeset -g -A key
+    key[F1]=${terminfo[kf1]}
+    key[F2]=${terminfo[kf2]}
+    key[F3]=${terminfo[kf3]}
+    key[F4]=${terminfo[kf4]}
+    key[F5]=${terminfo[kf5]}
+    key[F6]=${terminfo[kf6]}
+    key[F7]=${terminfo[kf7]}
+    key[F8]=${terminfo[kf8]}
+    key[F9]=${terminfo[kf9]}
+    key[F10]=${terminfo[kf10]}
+    key[F11]=${terminfo[kf11]}
+    key[F12]=${terminfo[kf12]}
     key[Backspace]=${terminfo[kbs]}
     key[Insert]=${terminfo[kich1]}
     key[Home]=${terminfo[khome]}
@@ -38,8 +50,25 @@ kbd_terminfo_init() {
     key[Down]=${terminfo[kcud1]}
     key[Left]=${terminfo[kcub1]}
     key[Right]=${terminfo[kcuf1]}
+    # Any ideas what to map this to?
+    key[Menu]=
+
+    # When the terminal has smkx and rmkx capabilities, set-up appropriate
+    # functions for ZLE to put the terminal into "application" mode.
+    if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} ))
+    then
+        function zle-line-init () {
+            echoti smkx
+        }
+        function zle-line-finish () {
+            echoti rmkx
+        }
+        zle -N zle-line-init
+        zle -N zle-line-finish
+    fi
 }
 
+# Default key bindings.
 kbd_defaults() {
     [[ -n ${key[Backspace]} ]] && bindkey "${key[Backspace]}" backward-delete-char
     [[ -n ${key[Insert]} ]] && bindkey "${key[Insert]}" overwrite-mode
@@ -55,6 +84,7 @@ kbd_defaults() {
 }
 
 kbd_bindkeys() {
+    kbd_defaults
     local bindings_file=${KBD_BINDINGS_FILE:-$ZDOTDIR/.zsh/kbd-bindings}
     if [[ -r $bindings_file ]]
     then
@@ -62,19 +92,21 @@ kbd_bindkeys() {
         do
             [[ -n ${key[$k]} ]] && bindkey "${key[$k]}" $b
         done < $bindings_file
-    else
-        kbd_defaults
     fi
 }
 
-case $KBD_CONFIG_METHOD in
-    zkbd)
-        kbd_zkbd_init
-        ;;
-    terminfo|*)
-        kbd_terminfo_init
-        ;;
-esac
+kbd_init() {
+    case $KBD_CONFIG_METHOD in
+        zkbd)
+            kbd_zkbd
+            ;;
+        terminfo|*)
+            kbd_terminfo
+            ;;
+    esac
 
-kbd_bindkeys
-unset key
+    kbd_bindkeys
+    unset key
+}
+
+kbd_init
