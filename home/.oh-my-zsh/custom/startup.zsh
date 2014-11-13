0c31c31fa0ba6805134be4af5bbb15c503e800ce
David Winter <dawi2332@gmail.com>
2014-11-13 23:28:26 +0100

    Synced .zshrc with oh-my-zsh's template. Moved startup-related stuff from .zshrc to $ZSH_CUSTOM/startup.zsh.

---
+++ home/.oh-my-zsh/custom/startup.zsh
@@ -0,0 +1,9 @@
+zsh-startup rc
+
+# Autoload all shell functions from all directories in $fpath (following
+# symlinks) that have the executable bit on (the executable bit is not
+# necessary, but gives you an easy way to stop the autoloading of a
+# particular shell function). $fpath should not be empty for this to work.
+for func in $^fpath/*(N-.x:t); autoload $func
+
+# vim: ft=zsh ts=8 st=4 sts=4 et
